151c023dad8d575e355daccee46c10ff9d9a0218
SIFY <154904386+Sify47@users.noreply.github.com>
2026-01-07 17:55:46 +0200

    Limit scraping to first 15 pages

--- scrape_data.py
+++ scrape_data.py
@@ -212,7 +212,7 @@ def main():
     all_properties = []
 
     # جمع من 3 صفحات
-    for page_num in range(1, 31):
+    for page_num in range(1, 16):
         if page_num == 1:
             page_url = base_url
         else:
